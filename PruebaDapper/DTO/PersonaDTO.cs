284933569a18f8066f31fcdeaffc33590d43717f
Adrian Alexander Requelme Campoverde <is-arequelme@bancoguayaquil.com>
2025-03-28 11:05:43 -0500

    se agg busqueda por ci

--- PruebaDapper/DTO/PersonaDTO.cs
+++ PruebaDapper/DTO/PersonaDTO.cs
@@ -1,16 +1,38 @@
-﻿namespace PruebaDapper.DTO
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PruebaDapper.DTO
 {
-    public class PersonaDTO
+    [Table("HCK_PERSONAS")]
+    public class HckPersona
     {
+        [Column("CEDULA")]
         public string Cedula { get; set; }
+
+        [Column("NOMBRES")]
         public string Nombres { get; set; }
+
+        [Column("APELLIDOS")]
         public string Apellidos { get; set; }
+
+        [Column("CIUDADANIA")]
         public string Ciudadania { get; set; }
-        public DateTime? FechaNacimiento { get; set; }
+
+        [Column("FECHA_NACIMIENTO")]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Column("ESTADO_CIVIL")]
         public string EstadoCivil { get; set; }
+
+        [Column("PROFESION")]
         public string Profesion { get; set; }
+
+        [Column("NIVEL_ESTUDIOS")]
         public string NivelEstudios { get; set; }
-        public int? EsCliente { get; set; }
+
+        [Column("ES_CLIENTE")]
+        public int EsCliente { get; set; }
+
+        [Column("TIPO_PERSONA")]
         public string TipoPersona { get; set; }
     }
 }
